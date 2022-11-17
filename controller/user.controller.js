61c3b7a6c85d7be4b765548d784154ecacf63fbd
preetiyadav30 <83328579+preetiyadav30@users.noreply.github.com>
2022-11-17 10:29:13 +0530

    chnage in avtar table

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -119,7 +119,7 @@ const signup = async (req, res, next) => {
 
 const avtar_category = async (req, res) => {
     try {
-        await db.query('Select * from Avtar', (err, results, feilds) => {
+        await db.query('Select * from avtar', (err, results, feilds) => {
             if (err) {
                 res.status(400).send({
                     success: false,
@@ -838,6 +838,7 @@ const admin_getQuestion=async(req,res,next)=>{
         })
     }
 }
+
 
 module.exports = {
     signup, avtar_category, add_question, question, add_avtar, admin_signup, get_question_user, admin_update_question, admin_add_category, admin_add_language, admin_delete_language, total_user, total_language, total_category, admin_update_questionStatus, adminLogin1, answer1, quiz_category, admin_Statistics,logout,admin_get_user,admin_getQuestion
